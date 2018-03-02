045daed255cd7ff6b3ec83ebd56e567e717670d9
Nathan Solomon <nbsolomon4559@gmail.com>
2018-03-02 17:36:24 -0600

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,199 @@
+{
+
+  "name": "english",
+
+  "global": {
+    "inv_usr": ":warning: I can't find user! Please, type user ID, or @<user>",
+    "no_permissions": ":warning: You have no permissions to use this command",
+    "bot_no_permissions": ":warning: I have no permissiosn to do it!",
+    "informations": "Informations",
+    "too_few_args": "Too few arguments! Type px!help <command> to get usage of this command",
+    "role_higher_than_bot": "This user has higher role than me!",
+    "user_role_lower": "This user has higher role than you!",
+    "inv_channel": ":x: Invalid channel!"
+  },
+
+  "weather": {
+    "valid_loc": "Please enter a valid location.",
+    "weather_for": "Weather for",
+    "timezone": "Timezone",
+    "degree_type": "Degree type",
+    "temp": "Temperature",
+    "degree": "degree",
+    "feels_like": "Feels Like",
+    "winds": "Winds",
+    "humidity": "Humidity",
+    "north": "North",
+    "southeast": "South-East",
+    "northeast": "North-East",
+    "southwest": "South-West",
+    "northwest": "North-West",
+    "south": "South",
+    "east": "East",
+    "west": "West"
+
+  },
+
+  "help": {
+    "user_grp": "User",
+    "showing_usr_cmds": "Currently showing user commands. To view another group do px!help [group / command]",
+    "cmds_found": "commands found",
+    "optional": "means optional",
+    "required": "means required"
+  },
+
+  "setlanguage": {
+    "invalid_lang": "Cannot find language! Please enter propper name\nExample: px@setlang english",
+    "lang_set_to": "Language set to `{lang}`",
+    "chan_lang_set_to": "Channel language set to ` {lang} `"
+  },
+
+  "serverinfo": {
+    "owner": "Owner",
+    "region": "Server region",
+    "channel_count": "Channel count",
+    "total_members": "Total members",
+    "humans": "Humans",
+    "bots": "Bots",
+    "verification_lvl": "Verification level",
+    "server_created": "Server created"
+  },
+
+  "userinfo": {
+    "status": "Status",
+    "online": "Online",
+    "offline": "Offline",
+    "afk": "Away From Keyboard (AFK)",
+    "dnd": "Do Not Disturb",
+    "roles": "Roles",
+    "nickname": "Nickname",
+    "no_nickname": "No nickname",
+    "username": "Username",
+    "acc_creation": "Account creation date"
+  },
+
+  "removelogchannel": {
+    "no_log_channel": ":x: This guild has no log channel configured!",
+    "success": ":white_check_mark: Successfully removed log channel!"
+  },
+
+  "removereportchannel": {
+    "no_report_channel": ":x: This guild has no report channel configured!",
+    "success": ":white_check_mark: Successfully removed report channel!"
+  },
+
+  "removewarnchannel": {
+    "no_warn_channel": ":x: This guild has no warn channel configured!",
+    "success": ":white_check_mark: Successfully removed warn channel!"
+  },
+
+  "removewelcomechannel": {
+    "no_welcome_channel": ":x: This guild has no welcome channel configured!",
+    "success": ":white_check_mark: Successfully removed welcome channel!"
+  },
+
+  "setautorole": {
+    "success": ":white_check_mark: Set role to give automatically on join to {role}"
+  },
+
+  "setleavemessage": {
+    "inv_msg": ":warning: Please enter valid message, for user name use `{USER}`",
+    "msg_set": "Leave message set to **{msg}**"
+  },
+
+  "setwelcomemessage": {
+    "inv_msg": ":warning: Please enter valid message, for user name use `{USER}`",
+    "msg_set": "Welcome message set to **{msg}**"
+  },
+
+  "setlogchannel": {
+    "chan_set": ":white_check_mark: Log channel set to {channel}"
+  },
+
+  "setreportchannel": {
+    "chan_set": ":white_check_mark: Report channel set to {channel}"
+  },
+
+  "setwarnchannel": {
+    "chan_set": ":white_check_mark: Warn channel set to {channel}"
+  },
+
+  "setwelcomechannel": {
+    "chan_set": ":white_check_mark: Welcome channel set to {channel}"
+  },
+
+  "setmaxwarns": {
+    "inv_amount": ":warning: You must specify value that you want to be maximum warn count before ban",
+    "success": ":white_check_mark: Successfully set maximum warn count before ban to {amount} warns"
+  },
+
+  "warn": {
+    "no_warn_channel": ":warning: This server has no warn channel configured, please contact administrator to run px@setwarnchannel"
+  },
+
+  "report": {
+    "no_report_channel": ":warning: This server has no report channel configured, please contact administrator to run px@setreportchannel",
+    "user": "Reported user",
+    "reported_by": "Reported by",
+    "chn": "Channel",
+    "time": "Time",
+    "rsn": "Reason",
+    "sent": ":white_check_mark: Your report has been sent successfully"
+  },
+
+  "botinfo": {
+    "author": "Author",
+    "version": "Version",
+    "website": "Website",
+    "cmd_count": "Command count",
+    "using_pixelbot": "Servers using PixelBOT",
+    "users_using_pixelbot": "Users using PixelBOT"
+  },
+
+  "fortnite": {
+    "lifetime": "Lifetime statistics",
+    "total_score": "Total score",
+    "matches_played": "Matches played",
+    "wins": "Wins",
+    "win_percentage": "Win percentage",
+    "kills": "Kills",
+    "killdeathratio": "K/D Ratio",
+    "kills_per_minute": "Kills per minute",
+    "time_played": "Time played",
+    "averrage_survival_time": "Averrage survival time",
+    "inv_usr": "Invalid user! Check if user uses platform that you selected!"
+  },
+
+  "langlist": {
+    "avb_lang_list": "List of available commands"
+  },
+
+  "mute": {
+    "mute_yourself": "You cannot mute yourself!",
+    "already_muted": ":warning: This user is already muted!",
+    "proper_time": ":warning: Please enter a proper time value!"
+  },
+
+  "purge": {
+    "num_as_args": "Please use a number as your arguments. \n Usage: `px!purge <amount>`",
+    "less_than": ":warning: Number of messages to delete should be less than or equal to 100",
+    "success": "Successfully deleted {amount} messages!"
+  },
+
+  "spotify": {
+    "title": "Spotify track info",
+    "name": "Song name",
+    "album": "Album",
+    "author": "Author",
+    "listen": "Listen to track",
+    "not_listening": "**This user isn't listening to Spotify!**"
+  },
+
+  "unmute": {
+    "cant_unmute_this_usr": ":warning: You cannot unmute that user!",
+    "cant_unmute_yourself": ":warning: You cannot unmute yourself!",
+    "not_muted": ":warning: This user is not muted!",
+    "success": ":mute: {user} has been unmuted"
+  }
+
+}
